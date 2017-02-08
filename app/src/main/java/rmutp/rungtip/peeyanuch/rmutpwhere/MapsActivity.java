d8b479acdd2a3d9be4b09d2f3ad8d65ebe2abfc1
peeyanuch <nanziiechaiyaporn@gmail.com>
2017-02-08 14:58:10 +0700

    find location of user ok

--- app/src/main/java/rmutp/rungtip/peeyanuch/rmutpwhere/MapsActivity.java
+++ app/src/main/java/rmutp/rungtip/peeyanuch/rmutpwhere/MapsActivity.java
@@ -1,7 +1,16 @@
 package rmutp.rungtip.peeyanuch.rmutpwhere;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,20 +21,131 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
     //Explicit
     private GoogleMap mMap;
-    private double rmutpLatADouble = 13.817895 ,rmutpLngADouble = 100.511889;
+    private double rmutpLatADouble = 13.817895, rmutpLngADouble = 100.511889;
     private LatLng rmutpLatLng;
+    private double userLatADouble = 0.0, userLngADouble = 0.0;
+    private LocationManager locationManager;
+    private Criteria criteria;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        //for setup
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     } //Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+            userLatADouble = networkLocation.getLatitude();
+            userLngADouble = networkLocation.getLongitude();
+        }
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            userLatADouble = gpsLocation.getLatitude();
+            userLngADouble = gpsLocation.getLongitude();
+        }
+
+        Log.d("8febV1","lat ==>" + userLatADouble);
+        Log.d("8febV1","lng ==>" + userLngADouble);
+
+        try {
+            LatLng latLng = new LatLng(userLatADouble, userLngADouble);
+            myCreateMarker(latLng, R.drawable.bird48); //เปลี่ยนรูป user
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }// onResume
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.removeUpdates(locationListener);
+
+
+
+    }
+
+    public Location myFindLocation(String strProvider) {
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        }   // if
+
+        return location;
+    }
+
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            userLatADouble = location.getLatitude();
+            userLngADouble = location.getLongitude();
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
 
 
@@ -37,7 +157,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         rmutpLatLng = new LatLng(rmutpLatADouble,rmutpLngADouble);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(rmutpLatLng,16));
 
-        myCreateMarker(rmutpLatLng, R.drawable.build4);
+        myCreateMarker(rmutpLatLng, R.drawable.build4); //เปลี่ยนรูปมาคเกอร์rmutp
 
 
     } //OnMap
