25d2d1ed17b08792ed92c04881988b8eb218e585
peeyanuch <nanziiechaiyaporn@gmail.com>
2017-02-08 13:51:53 +0700

    setup center rmutp

--- app/src/main/java/rmutp/rungtip/peeyanuch/rmutpwhere/MapsActivity.java
+++ app/src/main/java/rmutp/rungtip/peeyanuch/rmutpwhere/MapsActivity.java
@@ -11,8 +11,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    //Explicit
     private GoogleMap mMap;
+    private double rmutpLatADouble = 13.817895 ,rmutpLngADouble = 100.511889;
+    private LatLng rmutpLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +24,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+    } //Main Method
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+        //   for Setup Center Map
+        rmutpLatLng = new LatLng(rmutpLatADouble,rmutpLngADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(rmutpLatLng,16));
+
+
+
+    } //OnMap
+
+} //Main Class
