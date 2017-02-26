c991caec9f4b628fb4c2188642445c606983e8e7
masterUNG <phrombutr@gmail.com>
2017-02-26 13:19:24 +0700

    Search View Near OK

--- app/src/main/java/rmutp/rungtip/peeyanuch/rmutpwhere/Search.java
+++ app/src/main/java/rmutp/rungtip/peeyanuch/rmutpwhere/Search.java
@@ -3,15 +3,20 @@ package rmutp.rungtip.peeyanuch.rmutpwhere;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class Search extends AppCompatActivity implements View.OnClickListener {
+
+public class Search extends AppCompatActivity implements View.OnClickListener, SearchView.OnQueryTextListener {
 
     private ImageView bar1ImageView, bar2ImageView, bar3ImageView,
             bar4ImageView, bar5ImageView;
@@ -38,13 +43,39 @@ public class Search extends AppCompatActivity implements View.OnClickListener {
 
 
         //Image Controller
+        imageController();
+
+    }   // Main Method
+
+    private void imageController() {
         bar1ImageView.setOnClickListener(Search.this);
         bar2ImageView.setOnClickListener(Search.this);
         bar3ImageView.setOnClickListener(Search.this);
         bar4ImageView.setOnClickListener(Search.this);
         bar5ImageView.setOnClickListener(Search.this);
 
-    }   // Main Method
+    }
+
+    private void setupSearchView() {
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(Search.this);
+        mSearchView.setSubmitButtonEnabled(true);
+        mSearchView.setQueryHint(categorySearchStrings[index]);
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            mListView.clearTextFilter();
+        } else {
+            mListView.setFilterText(newText.toString());
+            Log.d("26febV2", "newText.toString() ==> " + newText.toString());
+        }
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
 
     private void createSearchView() {
 
@@ -55,6 +86,47 @@ public class Search extends AppCompatActivity implements View.OnClickListener {
             String strJSON = getDataFromServer.get();
 
             Log.d("26febV1", "JSON ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+
+            roomNumberStrings = new String[jsonArray.length()];
+            detailStrings = new String[jsonArray.length()];
+            buildStrings = new String[jsonArray.length()];
+            levelStrings = new String[jsonArray.length()];
+            imageStrings = new String[jsonArray.length()];
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                roomNumberStrings[i] = jsonObject.getString("roomnumber");
+                detailStrings[i] = jsonObject.getString("detail");
+                buildStrings[i] = jsonObject.getString("building");
+                levelStrings[i] = jsonObject.getString("level");
+                imageStrings[i] = jsonObject.getString("Image");
+
+            }   // for
+
+            String[] listSearch = new String[jsonArray.length()];
+            switch (index) {
+                case 0:
+                    listSearch = roomNumberStrings;
+                    break;
+                case 1:
+                    listSearch = detailStrings;
+                    break;
+                default:
+                    listSearch = roomNumberStrings;
+                    break;
+            }
+
+
+            mListView.setAdapter(new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1,
+                    listSearch));
+            mListView.setTextFilterEnabled(true);
+            setupSearchView();
+
 
         } catch (Exception e) {
             Log.d("26febV1", "e search ==> " + e.toString());
@@ -69,6 +141,8 @@ public class Search extends AppCompatActivity implements View.OnClickListener {
         bar4ImageView = (ImageView) findViewById(R.id.imageView9);
         bar5ImageView = (ImageView) findViewById(R.id.imageView10);
         textView = (TextView) findViewById(R.id.textView);
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+        mListView = (ListView) findViewById(R.id.list_viewSearch);
     }
 
 
